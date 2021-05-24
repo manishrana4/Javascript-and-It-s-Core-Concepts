630696cb3888a854d9c2d95f713793267b92843a
Rana <manishsingh.rana@cotiviti.com>
2021-05-24 13:37:43 +0545

    changes in spread operator

--- spreadOperator/spreadOperator.js
+++ spreadOperator/spreadOperator.js
@@ -1,28 +1,48 @@
 // Spread operator
 
 
-let a=["a","b","c","d"];
+let a = ["a", "b", "c", "d"];
 
-let b=[...a];
+let b = [...a];
 
-console.log("a,b:",a,b);
-
-b[0]="x";
-
-console.log("a,b:",a,b);
+console.log("a,b:", a, b);
 
 
-let x={
-    name:"harry",
-    age:"40"
+
+b[0] = "x";
+
+console.log("a,b:", a, b);
+
+
+let x = {
+    name: "harry",
+    age: "40"
 }
 
+let y = {
+    class: 12,
+    faculty: "Science",
+}
 
-let z={...x};
+let z = { ...x };
 
-console.log("x,z,",x,z);
 
-z.name="Patrick";
+console.log("x,z,", x, z);
 
-console.log("x,z,",x,z);
+let u={...x,...y};
 
+console.log("u:",u)
+
+z.name = "Patrick";
+
+console.log("x,z,", x, z);
+
+let numbers = [1, 2, 3];
+
+function add(x, y, z) {
+    return x + y + z;
+}
+
+let sum = add(...numbers);
+
+console.log("sum:", sum);
